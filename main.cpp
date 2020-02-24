45bfc13576ff9aee0c4d58ebfc75fc8e36693153
Rafael Arce Nazario <rafael.arce@upr.edu>
2020-02-24 10:42:27 -0400

    init

--- main.cpp
+++ main.cpp
@@ -7,4 +7,12 @@
 TEST_CASE( "ArrayList", "[ArrayList]" ) {
     ArrayList L;
     REQUIRE(L.getSize() == 0);
+
+    L.append(6);
+    L.append(9);
+    REQUIRE(L.getSize() == 2);
+    REQUIRE(L.at(0) == 6);
+    REQUIRE(L.at(1) == 9);
+
+
 }
